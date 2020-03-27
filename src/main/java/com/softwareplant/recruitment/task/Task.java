136e66af7e32dd40bba59fa3c20756b7272c7a8b
Bartłomiej Jańczak <bartlomiej.janczak@softwareplant.com>
2020-03-27 14:31:27 +0100

    Structure commit

---
+++ src/main/java/com/softwareplant/recruitment/task/Task.java
@@ -0,0 +1,8 @@
+package com.softwareplant.recruitment.task;
+
+public class Task {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/com/softwareplant/recruitment/task/TaskTest.java
@@ -0,0 +1,11 @@
+package com.softwareplant.recruitment.task;
+
+import org.testng.annotations.Test;
+
+public class TaskTest {
+
+	@Test
+	public void test() {
+		Task.main(new String[0]);
+	}
+}
